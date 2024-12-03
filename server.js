d2314d0452640b334b999623864af4f0b10aa43a
24HP2206 24HP2206 <24HP2206@admins-iMac.local>
2024-12-03 02:09:59 -0800

    fixes

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const userRoute = require('./Routes/userRoute.js')
 const postRoute = require('./Routes/postRoute.js')
 const commentRoute = require('./Routes/commentRoute.js')
 const likeRoute = require('./Routes/likeRoute.js')
+const cors = require("cors")
+
+
 dotenv.config()
 
 const app = express()
 const port = 8080
-
+app.use(cors())
 const connectDatabase = async() => {
     const res = await mongoose.connect(process.env.MONGODB_URI)
 
     if (res) console.log("db connected")
+    console.log(typeof(process.env.MONGODB_URI))
 }
 
 connectDatabase()
